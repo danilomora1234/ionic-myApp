814a1fb9913a2e465c37d7850e3c607d0a6e8736
danilomora1234 <danilomora114@gmail.com>
2021-03-13 15:15:43 -0500

    calculadora

--- src/app/calculadora/calculadora.page.ts
+++ src/app/calculadora/calculadora.page.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.page.html',
   styleUrls: ['./calculadora.page.scss'],
 })
-export class CalculadoraPage implements OnInit {
-
-  constructor() { }
-
+export class CalculadoraPage implements OnInit  {
+numero1=null;
+numero2=null;
+simbolo=null;
+suma:number;
+resta:number;
+dividir:number;
+multi:number;
+  constructor(public alertController: AlertController,) { }
   ngOnInit() {
   }
 
+  async guardar() {
+    if(this.numero1 ==" "  || this.numero1 == null){
+      const alertvacio = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: '',
+        message: 'LO SIENTO NO HA INGRESADO NUMEROS',
+        buttons: ['VALE'],
+      });
+      await alertvacio.present();
+    }else{
+        
+      if(this.simbolo=="+"){
+        
+        this.suma=this.numero1+this.numero2,
+        this.numero1="";
+        this.numero2="";
+        this.simbolo="";
+      }if(this.simbolo=="-"){
+        this.resta=this.numero1-this.numero2,
+        this.numero1="";
+        this.numero2="";
+        this.simbolo="";
+      }if(this.simbolo=="*"){
+        this.multi=this.numero1*this.numero2,
+        this.numero1="";
+        this.numero2="";
+        this.simbolo="";
+      }if(this.simbolo=="/"){
+        this.dividir=this.numero1/this.numero2,
+        this.numero1="";
+        this.numero2="";
+        this.simbolo="";
+      } 
+    }     
+   }
 }
+  
+
+
+
+
