c00fd2f469b61de3283489b3b48e305d7e1fdd7a
danilomora1234 <danilomora114@gmail.com>
2021-03-13 15:54:45 -0500

    operaciones

--- src/app/calculadora/calculadora.page.ts
+++ src/app/calculadora/calculadora.page.ts
@@ -9,47 +9,42 @@ export class CalculadoraPage implements OnInit  {
 numero1=null;
 numero2=null;
 simbolo=null;
-suma:number;
-resta:number;
-dividir:number;
-multi:number;
+suma=null;
+resta=null;
+dividir=null;
+multi=null;
   constructor(public alertController: AlertController,) { }
   ngOnInit() {
   }
 
   async guardar() {
-    if(this.numero1 ==" "  || this.numero1 == null){
+    
+    if(this.numero1 ==" "  || this.numero1 == null ){
       const alertvacio = await this.alertController.create({
         cssClass: 'my-custom-class',
-        header: '',
-        message: 'LO SIENTO NO HA INGRESADO NUMEROS',
+        header: 'AVISO',
+        message: 'NO HA INGRESADO DATOS O SIMBOLO',
         buttons: ['VALE'],
       });
       await alertvacio.present();
     }else{
         
       if(this.simbolo=="+"){
-        
-        this.suma=this.numero1+this.numero2,
-        this.numero1="";
-        this.numero2="";
-        this.simbolo="";
+        this.suma=parseInt(this.numero1)+parseInt(this.numero2)
+        console.log(this.suma)
       }if(this.simbolo=="-"){
-        this.resta=this.numero1-this.numero2,
-        this.numero1="";
-        this.numero2="";
-        this.simbolo="";
+        this.resta=this.numero1-this.numero2
+        console.log(this.resta)
       }if(this.simbolo=="*"){
-        this.multi=this.numero1*this.numero2,
-        this.numero1="";
-        this.numero2="";
-        this.simbolo="";
+        this.multi=this.numero1*this.numero2
+        console.log(this.multi)
       }if(this.simbolo=="/"){
-        this.dividir=this.numero1/this.numero2,
+        this.dividir=this.numero1/this.numero2
+        console.log(this.dividir)
+      } 
         this.numero1="";
         this.numero2="";
         this.simbolo="";
-      } 
     }     
    }
 }
